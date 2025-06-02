40adb436f9890dc73f12bc3244489b5496308bd5
cookeeindia <cookee.india@gmail.com>
2025-06-02 22:38:31 +0530

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('addSubjectBtn').addEventListener('click', () => {
-    alert('વિષય એડ કરવા માટેનું ફોર્મ અહીં આવશે.');
-  });
-});
