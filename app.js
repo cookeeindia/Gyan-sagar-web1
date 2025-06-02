bcbcdd9e7be4cff3bbe4ba63011635a945e647ee
cookeeindia <cookee.india@gmail.com>
2025-06-02 19:22:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('addSubjectBtn').addEventListener('click', () => {
+    alert('વિષય એડ કરવા માટેનું ફોર્મ અહીં આવશે.');
+  });
+});
