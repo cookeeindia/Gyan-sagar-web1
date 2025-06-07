0d0f05b6ea001fd45fd3d69e10001c1bd08ce0e0
cookeeindia <cookee.india@gmail.com>
2025-06-07 22:24:57 +0530

    Delete search.js

--- search.js
+++
@@ -1,4 +0,0 @@
-function searchTopics() {
-  const query = document.getElementById('searchInput').value.toLowerCase();
-  window.location.href = 'search-results.html?q=' + encodeURIComponent(query);
-}
